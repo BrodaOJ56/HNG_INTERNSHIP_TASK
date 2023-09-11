3f036051776e865a81cf595108fb419d233395c4
BrodaOJ56 <olujas1@gmail.com>
2023-09-12 00:57:55 +0100

    updated

--- hng_info_app/views.py
+++ hng_info_app/views.py
@@ -7,23 +7,37 @@ def get_info(request):
     slack_name = request.GET.get('slack_name')
     track = request.GET.get('track')
 
+    # Validate track format
+    if track != "backend":
+        return JsonResponse({
+            "error": "Invalid track format. Should be 'backend'",
+            "status_code": 400
+        })
+
     # Get current day of the week and UTC time
     utc_now = datetime.now(pytz.UTC)
     current_day_of_week = utc_now.strftime("%A")
     current_utc_time = utc_now.strftime("%Y-%m-%dT%H:%M:%SZ")
 
     # GitHub URLs
-    github_file_url = "https://github.com/BrodaOJ56/HNG_INTERNSHIP_TASK/blob/master/hng_info_project"
-    github_source_code_url = "https://github.com/BrodaOJ56/HNG_INTERNSHIP_TASK"
+    github_file_url = "https://github.com/BrodaOJ56/HNG_INTERNSHIP_TASK/blob/master/hng_info_app"
+    github_repo_url = "https://github.com/BrodaOJ56/HNG_INTERNSHIP_TASK"
+
+    # Check for required fields
+    if not slack_name or not current_day_of_week or not current_utc_time or not github_repo_url:
+        return JsonResponse({
+            "error": "Missing required field(s)",
+            "status_code": 400
+        })
 
     # Response data
     response_data = {
         "slack_name": slack_name,
-        "current_day_of_week": current_day_of_week,
-        "current_utc_time": current_utc_time,
+        "current_day": current_day_of_week,
+        "utc_time": current_utc_time,
         "track": track,
         "github_file_url": github_file_url,
-        "github_source_code_url": github_source_code_url,
+        "github_repo_url": github_repo_url,
         "status_code": 200
     }
 
